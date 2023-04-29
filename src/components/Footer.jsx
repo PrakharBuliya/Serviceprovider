1d6cbc70f836c9d02017337a171a6418d085760e
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-04-29 14:37:04 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>techystar</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://twitter.com/Prakhar50115657" target={"blank"}>
+            Twitter
+          </a>
+          <a href="https://github.com/PrakharBuliya" target={"blank"}>
+            Github
+          </a>
+          <a href="https://youtube.com" target={"blank"}>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
